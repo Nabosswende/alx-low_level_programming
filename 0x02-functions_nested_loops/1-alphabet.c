fa2789fdc03a0108fa3ced5a1a018ad803417753
Bintou DIMA <bintoudima16@gmail.com>
2022-03-16 21:45:07 +0000

    functions_nested_loops #1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet in lowercase
- * followed by a new line
- *
+ * print_alphabet - Entry point
  * Return:0
  */
 
-void print_alphabet()
+void print_alphabet(void)
 {
 	char c = 'a';
 
 	while (c <= 'z')
 	{
-
 		_putchar(c);
 		c++;
 
 	}
 	_putchar('\n');
+
 }
